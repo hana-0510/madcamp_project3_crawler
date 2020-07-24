ee379fd0c664006cf00652e82c401b96405a7421
Yeongil Yoon <askme143@kaist.ac.kr>
2020-07-25 00:34:31 +0900

    Success selecting

--- sample_crawler.py
+++ sample_crawler.py
@@ -16,8 +16,6 @@ if __name__ == '__main__':
     next_key_element = driver.find_element_by_css_selector('a.next._btn._btn_next.on')
 
     for i in range(total_num - 1):
-        file_data = OrderedDict()
-        
         title_selector = 'div.item_box > dl > dd.tit > a'
         period_selector = 'div.item_box > dl > dd.period'
         place_selector = 'div.item_box > dl > dd:nth-child(6) > a'
@@ -25,49 +23,59 @@ if __name__ == '__main__':
         reserv_url_selector = 'a.resevation'
         thumb_url_selector = 'div.img_box > a > img'
         
-        for j in range(2):
-            ul_nth = "ul:nth-child(" + j + ") "
-            for k in range(3):
-                li_nth = "li:nth-child(" + j + ") "
+        for j in range(3):
+            ul_nth = "ul:nth-child(" + str(j + 1) + ") > "
+            for k in range(2):
+                li_nth = "li:nth-child(" + str(k + 1) + ") "
                 
                 prefix = ul_nth + li_nth
                 
                 title_element = driver.find_elements_by_css_selector(prefix + title_selector)
-                period_element = driver.find_elements_by_css_selector(prefix + period_selector)
-                place_element = driver.find_elements_by_css_selector(prefix + place_selector)
-                place_url_element = driver.find_elements_by_css_selector(prefix + place_url_selector)
-                reserv_url_element = driver.find_elements_by_css_selector(prefix + reserv_url_selector)
-                thumb_url_element = driver.find_elements_by_css_selector(prefix + thumb_url_selector)
-                
                 if len(title_element) > 0:
+                    period_element = driver.find_elements_by_css_selector(prefix + period_selector)
+                    place_element = driver.find_elements_by_css_selector(prefix + place_selector)
+                    thumb_url_element = driver.find_elements_by_css_selector(prefix + thumb_url_selector)
+                    
+                    place_url_element = driver.find_elements_by_css_selector(prefix + place_url_selector)
+                    reserv_url_element = driver.find_elements_by_css_selector(prefix + reserv_url_selector)
+                    
                     title = title_element[0].text
                     period = period_element[0].text
                     place = place_element[0].text
-                    place_url = place_url_element[0].get_attribute('href') if len(place_url_element) > 0 else "null"
-                    reserv_url = reserv_url_element[0].get_attribute('href')  if len(reserv_url_element) > 0 else "null"
                     thumb_url = thumb_url_element[0].get_attribute('src')
                     
-                    file_data["title"] = title_element[0].text
-                    file_data[""]
-                
-                
-                
-        title_element_list = driver.find_elements_by_css_selector('div.item_box > dl > dd.tit > a')
-        period_element_list = driver.find_elements_by_css_selector('div.item_box > dl > dd.period')
-        place_element_list = driver.find_elements_by_css_selector('div.item_box > dl > dd:nth-child(6) > a')
-        place_key_element_list = driver.find_elements_by_css_selector('a.map')
-        place_key_element_list = driver.find_elements_by_css_selector('a.resevation')
-        thumb_url_element_list = driver.find_elements_by_css_selector('div.img_box > a > img')
-
-        for j in range(len(title_element_list)):
-            print('---------------------')
-            print(title_element_list[j].text)
-            print(period_element_list[j].text)
-            print(place_element_list[j].text)
-            print(place_key_element_list[j].get_attribute('href'))
-            print(thumb_url_element_list[j].get_attribute('src'))
-            print('---------------------')
-
+                    place_url = place_url_element[0].get_attribute('href') if len(place_url_element) > 0 else "null"
+                    reserv_url = reserv_url_element[0].get_attribute('href')  if len(reserv_url_element) > 0 else "null"
+                    
+                    period_split = period.split(" ~ ")
+                    start_split = period_split[0].split(".")
+                    finish_split = period_split[1].split(".")
+                    
+                    start_y, start_m, start_d, temp = tuple(start_split)
+                    finish_y, finish_m, finish_d, temp = tuple(finish_split)
+                    
+                    start_y, start_m, start_d = int(start_y), int(start_m), int(start_d)
+                    finish_y, finish_m, finish_d = int(finish_y), int(finish_m), int(finish_d)
+                    
+                    file_data = OrderedDict()
+                    
+                    file_data["title"] = title
+                    file_data["place"] = place
+                    file_data["thumb_url"] = thumb_url
+                    
+                    file_data["start_y"] = start_y
+                    file_data["start_m"] = start_m
+                    file_data["start_d"] = start_d
+                    
+                    file_data["finish_y"] = finish_y
+                    file_data["finish_m"] = finish_m
+                    file_data["finish_d"] = finish_d
+                    
+                    file_data["place_url"] = place_url
+                    file_data["reserv_url"] = reserv_url
+                    
+                    print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
+                else:
+                    print("Error or finished")
         next_key_element.click()
         time.sleep(1)
-    
